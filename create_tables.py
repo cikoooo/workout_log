e81285531ae05a42b993dc5d454c4b6384749712
cikoooo <54143309+cikoooo@users.noreply.github.com>
2020-08-17 14:55:22 +0300

    First commit

---
+++ create_tables.py
@@ -0,0 +1,72 @@
+'''
+Creating necessary tables for database
+'''
+
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    '''
+    Create a database connection to the SQLite database, specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    '''
+
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, create_table_sql):
+    '''
+    Create a table from sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    '''
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    
+    except Error as e:
+        print(e)
+
+def main():
+    database = r"C:\Users\Nico\Documents\tty\ohjelmointi\workout_log\workout.db"
+
+    sql_create_logs_table = '''CREATE TABLE IF NOT EXISTS logs (
+                                    id integer PRIMARY KEY,
+                                    date DATE NOT NULL,
+                                    workout text NOT NULL
+                                    );'''
+
+    sql_create_workout_table = '''CREATE TABLE IF NOT EXISTS workout (
+                                    id integer PRIMARY KEY,
+                                    workout_id integer NOT NULL,
+                                    workout text NOT NULL,
+                                    move text NOT NULL,
+                                    sets integer,
+                                    reps integer,
+                                    weight float,
+                                    extra text,
+                                    FOREIGN KEY (workout_id) REFERENCES logs (id)
+                                    );'''
+    
+    conn = create_connection(database) #Create a database connection
+
+    #Create tables
+    if conn is not None:
+        create_table(conn, sql_create_logs_table)
+        create_table(conn, sql_create_workout_table)
+
+    else:
+        print("Error! Cannot create database connection.")
+
+if __name__ == '__main__':
+    main()
---
+++ insert_data.py
@@ -0,0 +1,101 @@
+'''
+This allows inserting data to the database
+'''
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    '''
+    Create a databse connectino to the SQLite database, specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    '''
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_log(conn, log):
+    '''
+    Create a new log into the logs table
+    :param conn:
+    :param log:
+    :return: log id
+    '''
+
+    sql = '''INSERT INTO logs(date, workout)
+            VALUES(?, ?)'''
+
+    cur = conn.cursor()
+    cur.execute(sql, log)
+    conn.commit()
+    
+    return cur.lastrowid
+
+def create_workout(conn, workout):
+    '''
+    Create a workout to the workout table
+    :param conn:
+    :param workout:
+    :return:
+    '''
+
+    sql = '''INSERT INTO workout(workout_id, workout, move, sets, reps, weight, extra)
+            VALUES(?, ?, ?, ?, ?, ?, ?)'''
+
+    cur = conn.cursor()
+    cur.execute(sql, workout)
+    conn.commit()
+
+    return cur.lastrowid
+
+def main():
+    database = r"C:\Users\Nico\Documents\tty\ohjelmointi\workout_log\workout.db"
+
+    conn = create_connection(database) #Create a connection to the database
+    with conn:
+        #Create a new log
+        date = input('Insert date (format YYYY-MM-DD): ')
+        workout = input('Insert type of workout (chest/legs/back...): ')
+        log = (date, workout)
+        log_id = create_log(conn, log)
+
+        #Create new workouts
+        while True:
+            add_workout = input('Add another move? (Y/N): ')
+            if add_workout == 'Y':
+                move = input('Insert move: ')
+                
+                try:
+                    sets = int(input('Insert sets: '))
+                except ValueError:
+                        sets = int(input('Insert sets (integer): '))
+
+                try:
+                    reps = int(input('Insert reps: '))
+                except ValueError:
+                    reps = int(input('Insert reps (integer): '))
+
+                try:
+                    weight = float(input('Insert weight: '))
+                except ValueError:
+                    weight = float(input('Insert weight (float): '))
+
+                extra = input('More info?: ')
+
+                workout = (log_id, workout, move, sets, reps, weight, extra)
+                create_workout(conn, workout)
+                workout = ''
+
+            elif add_workout == 'N':
+                print("Logging workout...")
+                break
+
+
+        
+if __name__ == '__main__':
+    main()
