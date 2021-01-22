377348ae58aa4ba885afbe05dcf0e07b06a008ce
cikoooo <54143309+cikoooo@users.noreply.github.com>
2021-01-22 17:20:07 +0200

    Test-tiedostoon testi pandasin kanssa

--- test.py
+++ test.py
@@ -4,7 +4,20 @@ Pandas works nicely with sqlite
 '''
 import pandas as pd
 import sqlite3
+import matplotlib.pyplot as plt
 
 conn = sqlite3.connect('workout.db')
-test = pd.read_sql('SELECT * FROM gym WHERE "move" == "Penkki"', conn)
-print(test.head())
+test = pd.read_sql('SELECT * FROM gym', conn)
+paino = test['weight']
+testi = pd.read_sql('SELECT * FROM logs', conn)
+paiva = testi['date'][::-1]
+paino = paino[:len(paiva)]
+
+plt.plot(paiva, paino)
+plt.show()
+
+'''
+Alkeellinen testi.
+Piirtää sarjapainokehityksen, siten että päivä on x-akselilla ja paino y-akselilla.
+Päivät järjestetty "luonnolliseen" järjestykseen
+'''
