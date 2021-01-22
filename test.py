9e99e9a74ac3a4b232723486b6a04dac07b4c295
cikoooo <54143309+cikoooo@users.noreply.github.com>
2021-01-22 17:03:00 +0200

    Testing pandas with sqlite

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 '''
 For testing purposes
+Pandas works nicely with sqlite
 '''
+import pandas as pd
+import sqlite3
+
+conn = sqlite3.connect('workout.db')
+test = pd.read_sql('SELECT * FROM gym WHERE "move" == "Penkki"', conn)
+print(test.head())
