18bafa85ae278cee2bec6d7ba4cfadc6c110b0b4
cikoooo <54143309+cikoooo@users.noreply.github.com>
2021-01-24 19:04:18 +0200

    Uuden moduulin luonti, jonka avulla siirtää tietoja csv:stä tietokantaan

--- test.py
+++ test.py
@@ -5,22 +5,22 @@ Pandas works nicely with sqlite
 import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
+import datetime
+from datetime import date
 from insert_data import *
 
 conn = sqlite3.connect('workout.db')
-test = pd.read_sql('SELECT * FROM gym', conn)
-paino = test['weight']
-testi = pd.read_sql('SELECT * FROM logs', conn)
-paiva = testi['date'][::-1]
-paino = paino[:len(paiva)]
-
-plt.plot(paiva, paino)
-plt.show()
-
-# insert_data() #tämä käynnistää insert_data:n
 
 '''
 Alkeellinen testi.
 Piirtää sarjapainokehityksen, siten että päivä on x-akselilla ja paino y-akselilla.
 Päivät järjestetty "luonnolliseen" järjestykseen
 '''
+
+painot = pd.read_sql('SELECT AVG(weight) FROM gym WHERE move = "Penkki" GROUP BY workout_id', conn)
+paivat = pd.read_sql('SELECT logs.date FROM logs INNER JOIN gym ON logs.id = gym.workout_id WHERE gym.move ="Penkki" GROUP BY logs.id', conn)
+print(len(painot), len(paivat))
+plt.scatter(paivat['date'], painot['AVG(weight)'])
+plt.show()
+
+# insert_data() #tämä käynnistää insert_data:n
